35f645861463e5c127718d55fbd8e3e312e42bd2
lihaoxiang1989 <c.franten@gmail.com>
2017-11-27 11:36:41 -0600

    WriteToFile updated
    add private variable and private checking functions

--- WriteToFile/Log.php
+++ WriteToFile/Log.php
@@ -2,6 +2,9 @@
 
 class Log
 {
+    private $_Filename;
+    private $_Data;
+
 	/**
 	 * Write to a file
 	 * @param  str $filename  The name of the file
@@ -10,10 +13,15 @@ class Log
 	 */
     public function write($filename, $strData)
     {
-    	if (!is_writable($filename)) {
-    		die('Change your CHMOD permission to' . $filename);
-    	}
+        // Set Class Vars
+        $this->_Filename = $filename;
+        $this->_Data = $strData;
 
+    	// Check Data
+        $this->_CheckPermission();
+        $this->_CheckData(); 
+
+        // Write to File
     	$handle = fopen($filename, 'a+');
     	fwrite($handle, "\r\n" . $strData);
     	fclose($handle);
@@ -26,8 +34,32 @@ class Log
      */
     public function read($filename)
     {
+        $this->_Filename = $filename;
+        $this->_CheckExists();
+
     	$handle = fopen($filename, "r");
 
     	return file_get_contents($filename);
+    }
+
+    private function _CheckPermission()
+    {
+        if (!is_writable($this->_Filename)) {
+            die('Change your CHMOD permission to' . $this->_Filename);
+        }
+    }
+
+    private function _CheckData()
+    {
+        if (strlen($this->_Data) < 1) {
+            die('You must have at least one character.');
+        }
+    }
+
+    private function _CheckExists()
+    {
+        if (!file_exists($this->_Filename)) {
+            die('This file dose not exist');
+        }
     }
 }
